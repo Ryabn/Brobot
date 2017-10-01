3caade923524941855c043b06260e1179c1b3215
Ryan Yang <javathon@gmail.com>
2017-10-01 01:16:13 -0700

    leg walking forward works

--- robotLeg/robotLeg.ino
+++ robotLeg/robotLeg.ino
@@ -3,8 +3,8 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  400 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  350
+#define SERVOMAX  625
 
 void setup() {
   Serial.begin(9600);
@@ -17,24 +17,27 @@ void setup() {
 }
 
 void startWalking(){
-    
+    pwm.setPWM(2, 0, 160);
+    pwm.setPWM(3, 0, 420);
+    //420 lul
 }
 void stopWalking(){
-    
+    pwm.setPWM(2, 0, 250);
+    pwm.setPWM(3, 0, 550);
 }
 void loop() {
+    startWalking();
+    delay(500);
     for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
         pwm.setPWM(1, 0, pulselen);
-        pwm.setPWM(2, 0, pulselen-300);
-        pwm.setPWM(3, 0, pulselen-150);
     }
-    delay(2000);
-    /*
+    delay(500);
+    stopWalking();
+    delay(500);
     for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-        //pwm.setPWM(1, 0, pulselen);
-        //pwm.setPWM(2, 0, pulselen-300);
-        pwm.setPWM(3, 0, pulselen-150);
+        pwm.setPWM(1, 0, pulselen);
+
     }
-    delay(2000);
-    */
+    delay(500);
+    
 }
